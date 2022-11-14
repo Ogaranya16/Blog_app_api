3580fc19180ebec94e512fde116cc2e9eb6a3baf
Tochukwu <chitonwagu@gmail.com>
2022-11-14 10:41:27 +0100

    tried to resolve heroku error

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api/articles", articleRoute)
 app.use("/api/home", homeRoute)
 app.use("/api/draft", draftRoute)
 
-giapp.get('/', (req, res) => res.send ('home'));
+app.get('/', (req, res) => res.send ('home'));
 // app.get('/users', {verifyToken},(req, res)=> res.send('user'))
 // app.useauthRoute
 
